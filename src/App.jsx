875ace3f1d78e05441c8952e52e57562eda2b8e6
aiXpertLab <aiXpertLab@gmail.com>
2024-04-12 18:37:40 -0400

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '/images/nota_logo_2.jpg'
+import viteLogo from '/images/nota_logo_1.jpg'
+import './css/App.css'
+import Note from './components/Note'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(props) {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...') 
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="https://hypech.com" target="_blank"><img src={viteLogo} className="logo" alt="Vite logo" /></a>
+        <a href="https://hypech.com" target="_blank"><img src={reactLogo} className="logo react" alt="React logo" /></a>
       </div>
-      <h1>Vite + React</h1>
+      
+      <h1>Nota: Focus on what's essential.</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <ul>
+          {notes.map(note => 
+            <Note key={note.id} note={note} />
+          )}
+        </ul>
+
+        <form onSubmit={addNote} className="chat-form">
+          
+          <textarea 
+            placeholder="Type a note..."
+            value={newNote}
+            onChange={handleNoteChange}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && !e.shiftKey) {
+                addNote(); // Call addNote function on Enter key press
+              }}}
+            rows={5} // Adjust the number of rows as needed
+          />
+          <button type="submit">Send</button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <p className="read-the-docs">The minimalist note-taking app for daily tasks and journaling. Write, remember, forget nothing.</p>
+
+      <div>
+      </div>
+
     </>
   )
 }
+
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './css/index.css'
+
+const notes = [
+  {id: 1,    content: 'HTML is easy',    important: true  },
+  {id: 2,    content: 'Browser can execute only JavaScript',    important: false  },
+  {id: 3,    content: 'GET and POST are the most important methods of HTTP protocol The minimalist note-taking app for daily tasks and journaling. Write, remember, forget nothing. The minimalist note-taking app for daily tasks and journaling. Write, remember, forget nothing. ',    important: true  }
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App notes = {notes}/>
 )
